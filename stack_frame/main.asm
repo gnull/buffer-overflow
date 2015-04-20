20b3083761ce391f2ef10f2aa703ab29ae171452
Ivan Oleynikov <ivan.oleynikov95@gmail.com>
2015-04-20 14:40:17 +0300

    objdump --no-show-raw-insn

--- stack_frame/main.asm
+++ stack_frame/main.asm
@@ -1,23 +1,24 @@
-80483ec <sol>:
-80483ec:		55                   	push   %ebp
-80483ed:		89 e5                	mov    %esp,%ebp
-80483ef:		8b 45 0c             	mov    0xc(%ebp),%eax
-80483f2:		8b 55 08             	mov    0x8(%ebp),%edx
-80483f5:		01 d0                	add    %edx,%eax
-80483f7:		5d                   	pop    %ebp
-80483f8:		c3                   	ret
+080483ec <sol>:
+80483ec:		push   %ebp
+80483ed:		mov    %esp,%ebp
+80483ef:		mov    0xc(%ebp),%eax
+80483f2:		mov    0x8(%ebp),%edx
+80483f5:		add    %edx,%eax
+80483f7:		pop    %ebp
+80483f8:		ret
 
-80483f9 <main>:
-80483f9:		55                   	push   %ebp
-80483fa:		89 e5                	mov    %esp,%ebp
-80483fc:		83 ec 18             	sub    $0x18,%esp
-80483ff:		c7 45 fc 01 00 00 00 	movl   $0x1,-0x4(%ebp)
-8048406:		c7 45 f8 02 00 00 00 	movl   $0x2,-0x8(%ebp)
-804840d:		8b 45 f8             	mov    -0x8(%ebp),%eax
-8048410:		89 44 24 04          	mov    %eax,0x4(%esp)
-8048414:		8b 45 fc             	mov    -0x4(%ebp),%eax
-8048417:		89 04 24             	mov    %eax,(%esp)
-804841a:		e8 cd ff ff ff       	call   80483ec <sol>
-804841f:		89 45 f4             	mov    %eax,-0xc(%ebp)
-8048422:		c9                   	leave
-8048423:		c3                   	ret
+080483f9 <main>:
+80483f9:		push   %ebp
+80483fa:		mov    %esp,%ebp
+80483fc:		sub    $0x18,%esp
+80483ff:		movl   $0x1,-0x4(%ebp)
+8048406:		movl   $0x2,-0x8(%ebp)
+804840d:		mov    -0x8(%ebp),%eax
+8048410:		mov    %eax,0x4(%esp)
+8048414:		mov    -0x4(%ebp),%eax
+8048417:		mov    %eax,(%esp)
+804841a:		call   80483ec <sol>
+804841f:		mov    %eax,-0xc(%ebp)
+8048422:		leave
+8048423:		ret
+	
